588a61565cebb95dedd3971aa8ab02963b9da8f2
jaceituno16 <javiacei@ucm.es>
2025-05-26 13:01:29 +0200

    feat: register

--- src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -3,10 +3,14 @@ package es.ucm.fdi.iw.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import io.micrometer.common.lang.Nullable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,9 +20,13 @@ import es.ucm.fdi.iw.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.transaction.Transactional;
 
 /**
- *  Non-authenticated requests only.
+ * Non-authenticated requests only.
  */
 @Controller
 public class RootController {
@@ -26,16 +34,19 @@ public class RootController {
     private static final Logger log = LogManager.getLogger(RootController.class);
 
     @Autowired
-	private EntityManager entityManager;
+    private EntityManager entityManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @ModelAttribute
-    public void populateModel(HttpSession session, Model model) {        
-        for (String name : new String[] {"u", "url", "ws"}) {
+    public void populateModel(HttpSession session, Model model) {
+        for (String name : new String[] { "u", "url", "ws" }) {
             model.addAttribute(name, session.getAttribute(name));
         }
     }
 
-	@GetMapping("/login")
+    @GetMapping("/login")
     public String login(Model model, HttpServletRequest request) {
         boolean error = request.getQueryString() != null && request.getQueryString().indexOf("error") != -1;
         model.addAttribute("loginError", error);
@@ -43,13 +54,87 @@ public class RootController {
     }
 
     @GetMapping("/register")
-    public String register(Model model, HttpServletRequest request) {
-        boolean error = request.getQueryString() != null && request.getQueryString().indexOf("error") != -1;
-        model.addAttribute("loginError", error);
+    public String register(Model model) {
         return "register";
     }
 
-	@GetMapping("/")
+    @PostMapping("confirm-register")
+    @Transactional
+    public String postRegister(Model model,
+            @RequestParam @Nullable String username,
+            @RequestParam @Nullable String password,
+            @RequestParam @Nullable String repeatPassword,
+            @RequestParam @Nullable String role,
+            @RequestParam @Nullable String telephone,
+            @RequestParam @Nullable String email,
+            @RequestParam @Nullable String dni,
+            @RequestParam @Nullable String firstName,
+            @RequestParam @Nullable String secondName,
+            @RequestParam @Nullable String name,
+            @RequestParam @Nullable String cif,
+            RedirectAttributes redirectAttributes) {
+
+        if (!password.equals(repeatPassword)) {
+            redirectAttributes.addFlashAttribute("error", "Las contraseñas no coinciden");
+            return "redirect:/register";
+        }
+
+        List<User> users = entityManager.createNamedQuery("User.byUsername", User.class)
+                .setParameter("username", username)
+                .getResultList();
+
+        if (users.size() > 0) {
+            redirectAttributes.addFlashAttribute("error", "Nombre de usuario existente");
+            return "redirect:/register";
+        }
+
+        User.Role r = User.Role.valueOf(role);
+        switch (r) {
+            case ENTERPRISE:
+                Enterprise enterprise = new Enterprise();
+
+                enterprise.setUsername(username);
+                enterprise.setPassword(passwordEncoder.encode(password));
+                enterprise.setEnabled(true);
+                enterprise.setRole(r);
+                enterprise.setWallet(0);
+                enterprise.setTelephone(Integer.parseInt(telephone));
+                enterprise.setEmail(email);
+                enterprise.setName(name);
+                enterprise.setCIF(cif);
+                
+                entityManager.persist(enterprise);
+                
+                break;
+
+            case USER:
+                Parker parker = new Parker();
+
+                parker.setUsername(username);
+                parker.setPassword(passwordEncoder.encode(password));
+                parker.setEnabled(true);
+                parker.setRole(r);
+                parker.setWallet(0);
+                parker.setTelephone(Integer.parseInt(telephone));
+                parker.setEmail(email);
+                parker.setDNI(dni);
+                parker.setFirstName(firstName);
+                parker.setSecondName(secondName);
+
+                entityManager.persist(parker);
+                
+                break;
+
+            default:
+                break;
+        }
+
+        redirectAttributes.addFlashAttribute("success", "Usuario registrado con exito!");
+        
+        return "redirect:/login";
+    }
+
+    @GetMapping("/")
     public String index(Model model) {
         return "index";
     }
